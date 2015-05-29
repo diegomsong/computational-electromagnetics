2aecfbb553f3d0aa2015793558e0ab4892650850
infinity.ethereal <infinity.ethereal@0d88c5ae-8e33-f000-d3c1-fbb3733563b2>
2015-05-29 05:44:50 +0000

    [FDTD] - Created lossy 1D wave propagation template file.

---
+++ Finite.Difference.Time.Domain/Problems/FDTD.1D.Wave.Propagation/One_Dimension_Wave_Propagation_Lossy.m
@@ -0,0 +1,48 @@
+% Simulation parameters.
+SIZE = 128; % No. of spatial steps
+MaxTime = SIZE; % No. of time steps
+PulseWidth = SIZE/4; % Controls width of Gaussian Pulse
+td = PulseWidth; % Temporal delay in pulse.
+imp0 = 377.0; % Impedence of free space
+source = 10; % Location of source
+
+% Constants.
+c = 3e8;
+pi = 3.141592654;
+e0 = (1e-9)/(36*pi);
+u0 = (1e-7)*4*pi;
+
+dt = 1e-12;
+dz = 3e-4;
+Sc = c * dt/dz
+
+% Initialization.
+Ex = zeros ( SIZE, MaxTime ); % x-component of E-field
+Hy = zeros ( SIZE, MaxTime ); % y-component of H-field
+PLOT1(1) = 0; % Data for plotting.
+
+% Outer loop for time-stepping.
+tic
+for q = 2:MaxTime
+    % Calculation of Hy using update difference equation for Hy. This is time step q.
+    Hy(1:SIZE-1,q) = Hy(1:SIZE-1,q-1) + ( ( Ex(1:SIZE-1,q-1) - Ex(2:SIZE,q-1) ) * dt/(u0*dz) );
+    % ABC for H at SIZE.
+    Hy(SIZE,q) = Hy(SIZE-1,q-1) + (Sc-1)/(Sc+1)*(Hy(SIZE-1,q) - Hy(SIZE,q-1) );
+    
+    % Calculation of Ex using updated difference equation for Ex. This is time step q+1/2.
+    Ex(2:SIZE,q) = Ex(2:SIZE, q-1) + ( dt/(e0*dz)*(Hy(1:SIZE-1, q) - Hy(2:SIZE, q)) );
+    % ABC for E at 1.
+    Ex(1,q) = Ex(2,q-1) + (Sc-1)/(Sc+1)*(Ex(2,q) - Ex(2,q-1));
+    
+    % Activating a plane-wave source.
+    Ex(source,q) = Ex(source,q) + exp( -1*((q-td)/(PulseWidth/4))^2 ) * Sc;
+end
+toc
+% Simulation animation.
+for i=1:MaxTime
+    figure (2)
+    plot ( Ex(:,i) )
+    axis([0 SIZE -0.6 0.6])
+    xlabel('Spatial step (k)')
+    ylabel('Electric field (Ex)')
+end
